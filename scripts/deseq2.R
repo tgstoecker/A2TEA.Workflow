2162cdd267782afc7c31c4df83d9722f46d96091
Tyll Stöcker <53227400+tgstoecker@users.noreply.github.com>
2020-06-13 14:55:11 +0200

    Delete deseq2.R

--- scripts/deseq2.R
+++
@@ -1,9 +0,0 @@
-library("DESeq2")
-
-
-dds_snakemake.wildcards <- readRDS(snakemake@input[[1]])
-
-dea_snakemake.wildcards <- DESeq(dds_snakemake.wildcards)
-
-saveRDS(dea_snakemake.wildcards, file=snakemake@output[[1]])
-
