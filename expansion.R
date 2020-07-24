efb4799c7dc99f3a85dd7f5e514365693ef7d2fa
tgstoecker <stoecker@cs02.cropbio.uni-bonn.de>
2020-07-25 00:01:16 +0200

    expansion analysis almost done; snakemake rule seems stable; started integrating jupyter tests into expansion.R script

---
+++ expansion.R
@@ -0,0 +1,11 @@
+num = snakemake@params[["num"]]
+num
+
+name = snakemake@params[["name"]]
+name
+
+expanded_in = snakemake@params[["expansion"]]
+expanded_in
+
+compared_to = snakemake@params[["comparison"]]
+compared_to
