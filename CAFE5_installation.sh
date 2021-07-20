601780688d87eef8b3f626dee340a38a4dfc122a
Tyll Stoecker <stoecker@cs03.cropbio.uni-bonn.de>
2021-07-20 21:30:47 +0000

    changes to CAFE5 installation to increase stability; cafe5_compile.yml contains environment

--- CAFE5_installation.sh
+++ CAFE5_installation.sh
@@ -3,11 +3,14 @@
 # option 1 (you have a modern machine / slash sudo rights)
 # avoid interferences by mini/conda
 # use - conda deactivate - to avoid using conda's installs of gnu libs
-# then comment out the first line of option 2 and run the script
 
 # Option 2 - THIS IS THE RECOMMENDED WAY!
 # use conda/mamba and install the compilers metapackage
-conda install -c conda-forge compilers
+# can be tricky - cblas.h problems with system vs miniconda install...
+#mamba install -c anaconda gcc_linux-64
+#mamba install -c conda-forge compilers
+#mamba install -c anaconda openblas-devel
+
 git clone https://github.com/tgstoecker/CAFE5
 cd CAFE5/
 autoconf
